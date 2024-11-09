55f96e74ce9851b468d80f01ef52c91b9050c337
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-11-09 19:30:58 +0100

    Print errors in script event handlers

--- scripting/script_event_dispatcher.gd
+++ scripting/script_event_dispatcher.gd
@@ -4,13 +4,20 @@ extends Node
 var _handlers: Array[LuauFunction] = []
 
 func _lua_Connect(pvm: LuauVM):
-    if not pvm.lua_isfunction(-1):
-        pvm.luaL_error("expected a function")
-        return 0
-    var function = pvm.lua_tofunction(-1)
-    _handlers.append(function)
-    return 0
+	if not pvm.lua_isfunction(-1):
+		pvm.luaL_error("expected a function")
+		return 0
+	var function = pvm.lua_tofunction(-1)
+	_handlers.append(function)
+	return 0
 
 func invoke(args: Array):
-    for handler in _handlers:
-        handler.pcallv(args)
+	var errors = []
+	for handler in _handlers:
+		var result = handler.pcallv(args)
+		var error = result.get_error()
+		if error and error is String and error != "<null>":
+			errors.append(error)
+	for error in errors:
+		print_rich("[color=red]Error in %s event handler: %s[/color]" % [name, error])
+	return errors
